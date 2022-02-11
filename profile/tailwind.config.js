840998e1b108eb346a019e21cfb206c87e6f2305
devmaster313 <abdullahqasemi2019@gmail.com>
2022-02-11 15:24:33 +0430

    changes

---
+++ profile/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ["Futura", "ui-sans-serif", "system-ui"],
+			serif: ["ui-serif", "Georgia"],
+			mono: ["ui-monospace", "SFMono-Regular"],
+			display: ["Oswald"],
+			body: ['"Open Sans"'],
+		},
+	},
+	plugins: [],
+};
